5d7e5953ebec1ffdecc06160917de79ba88da22b
Clément Murzeau <clement@studiorelief.fr>
2023-09-27 15:55:32 +0200

    build branch-1.0.5
    Fix scroll

--- src/utils/navbar.ts
+++ src/utils/navbar.ts
@@ -1,58 +1,35 @@
-/* function scrollNav() {
-  let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-  window.addEventListener('scroll', () => {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    const sectionNavbar = document.querySelector('.section_navbar');
-    const navbarButton = document.querySelector('.navbar_button-w'); // Sélection de .navbar_button-w seulement
-    const navbarBrand = document.querySelector('.navbar_brand-w'); // Sélection de .navbar_brand-w seulement
-
-    if (!sectionNavbar || !navbarButton || !navbarBrand) return; // Vérification de l'existence des éléments
-
-    // Si l'utilisateur défile vers le bas
-    if (scrollTop > lastScrollTop) {
-      (sectionNavbar as HTMLElement).style.transform = 'translateY(-5.125rem)';
-      (navbarButton as HTMLElement).style.transform = 'translateY(5.125rem)';
-      (navbarBrand as HTMLElement).style.transform = 'translateY(5rem)';
-    } else if (scrollTop < lastScrollTop) {
-      (sectionNavbar as HTMLElement).style.transform = 'translateY(0)';
-      (navbarButton as HTMLElement).style.transform = 'translateY(0)';
-      (navbarBrand as HTMLElement).style.transform = 'translateY(0)';
-    }
-
-    lastScrollTop = scrollTop; // Mise à jour de la dernière position de défilement
-  });
-}
-
-export { scrollNav }; */
-
 function scrollNav() {
   let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   window.addEventListener('scroll', () => {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    const sectionNavbar = document.querySelector('.section_navbar');
-    const navbarButton = document.querySelector('.navbar_button-w');
-    const navbarBrand = document.querySelector('.navbar_brand-w');
-    const navbarBrandImg = document.querySelector('.navbar_brand-img'); // Nouvel élément
-    const navbarBrandImgScroll = document.querySelector('.navbar_brand-img-scroll'); // Nouvel élément
+    const sectionNavbar = document.querySelector('.section_navbar') as HTMLElement; // Ajouté "as HTMLElement"
+    const navbarButton = document.querySelector('.navbar_button-w') as HTMLElement;
+    const navbarBrand = document.querySelector('.navbar_brand-w') as HTMLElement;
+    const navbarBrandImg = document.querySelector('.navbar_brand-img') as HTMLElement;
+    const navbarBrandImgScroll = document.querySelector('.navbar_brand-img-scroll') as HTMLElement;
 
     if (!sectionNavbar || !navbarButton || !navbarBrand || !navbarBrandImg || !navbarBrandImgScroll)
       return;
 
+    // Conversion de 2rem en pixels (en supposant que la taille de base est de 16px)
+    const twoRemInPixels = 1 * 16;
+
     // Si l'utilisateur défile vers le bas
     if (scrollTop > lastScrollTop) {
-      (sectionNavbar as HTMLElement).style.transform = 'translateY(-5.125rem)';
-      (navbarButton as HTMLElement).style.transform = 'translateY(5.125rem)';
-      (navbarBrand as HTMLElement).style.transform = 'translateY(5rem)';
-      (navbarBrandImg as HTMLElement).style.display = 'none'; // Cache le logo
-      (navbarBrandImgScroll as HTMLElement).style.display = 'block'; // Affiche le logo de défilement
-    } else if (scrollTop < lastScrollTop) {
-      (sectionNavbar as HTMLElement).style.transform = 'translateY(0)';
-      (navbarButton as HTMLElement).style.transform = 'translateY(0)';
-      (navbarBrand as HTMLElement).style.transform = 'translateY(0)';
-      (navbarBrandImg as HTMLElement).style.display = 'block'; // Affiche le logo
-      (navbarBrandImgScroll as HTMLElement).style.display = 'none'; // Cache le logo de défilement
+      sectionNavbar.style.transform = 'translateY(-5.125rem)';
+      navbarButton.style.transform = 'translateY(5.125rem)';
+      navbarBrand.style.transform = 'translateY(5rem)';
+      navbarBrandImg.style.display = 'none';
+      navbarBrandImgScroll.style.display = 'block';
+    }
+    // Si l'utilisateur défile vers le haut et scrollTop > 2rem
+    else if (scrollTop + twoRemInPixels < lastScrollTop) {
+      sectionNavbar.style.transform = 'translateY(0)';
+      navbarButton.style.transform = 'translateY(0)';
+      navbarBrand.style.transform = 'translateY(0)';
+      navbarBrandImg.style.display = 'block';
+      navbarBrandImgScroll.style.display = 'none';
     }
 
     lastScrollTop = scrollTop;
